587acdc69b0007bba2f7a96e81828c0d416c181a
Raven <avalon1610@gmail.com>
2014-01-07 18:32:47 +0800

    fix test code

--- program/main.c
+++ program/main.c
@@ -32,19 +32,8 @@ void Uninit()
 
 int main()
 { 
-	struct _MISSION *mission;
-
     Init();
-    //run_server();
-
-	mission = (MISSION *)malloc(sizeof(MISSION));
-	mission->type=ARP_CACHE_SATURATION_STORM;
-	mission->status=RUNNING;
-	 pthread_rwlock_wrlock(&rwlock);
-    InsertTailList(&mission_list,&mission->node);
-    pthread_rwlock_unlock(&rwlock);
-	start_test();
-	
+    run_server();
     Uninit();
     return 0;
 }
