ff5d0e7d2f7f5c65f265b52ee41263fc6463fba6
Raven <avalon1610@gmail.com>
2014-01-08 18:48:59 +0800

    add order struct and command_control

--- comm/comm.c
+++ comm/comm.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "comm.h"
 
 int DbgPrint(const char *format,...)
 {
-//#ifdef DEBUG
+#ifdef DEBUG
     va_list args;
     va_start(args,format);
     vprintf(format,args);
     va_end(args);
-//#endif
+#endif
     return 0;
+}
+
+int command_control(COMMAND cmd)
+{
+    switch(cmd.type)
+    {
+        case SCAN:
+            break;
+        case MISSION:
+            switch(cmd.order):
+            {
+                case START:
+                case RESUME:
+                    if (Running.running_thread)
+                    pthread
+                    break;
+                case PAUSE:
+                case STOP:
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
+
 }
--- include/comm.h
+++ include/comm.h
@@ -25,6 +25,14 @@ typedef enum _MISSION_STATUS
     ERROR
 } MISSION_STATUS;
 
+typedef enum _SCAN_STATUS
+{
+    RUNNING = 0,
+    PAUSE,
+    STOP,
+    ERROR
+} SCAN_STATUS;
+
 typedef struct _MISSION
 {
     LIST_ENTRY node;
@@ -38,12 +46,40 @@ typedef struct _ENV {
     unsigned char host[16];
     unsigned char target1[16];
     unsigned char target2[16];
-    unsigned char src_mac[6];
-    unsigned char dst_mac[6];
+    unsigned char host_mac[6];
+    unsigned char target1_mac[6];
+    unsigned char target2_mac[6];
 } ENV,*PENV; 
 
+typedef enum _COMMAND_TYPE
+{
+    SCAN = 0,
+    MISSION,
+    ANALYIZE
+} COMMAND_TYPE;
+
+typedef enum _COMMAND_ORDER
+{
+    START = 0,
+    PAUSE,
+    RESUME,
+    STOP
+} COMMAND_ORDER;
+
+typedef struct _COMMAND
+{
+    COMMAND_TYPE type;
+    COMMAND_ORDER order;
+    union
+    {
+        MISSION_TYPE m_type;
+        SCAN_TYPE s_type;
+    }
+    bool ALL; 
+} COMMAND,*PCOMMAND;
 
 int DbgPrint(const char *format,...);
+int command_control(COMMAND cmd);
 
 #ifdef __cplusplus
 };
